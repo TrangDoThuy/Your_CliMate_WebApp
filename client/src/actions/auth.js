d7fb5cc6d77063a3a40a253d73645409602beb62
Emma Thuy Trang DO <30380242+TrangDoThuy@users.noreply.github.com>
2021-02-10 14:13:55 +0800

    apply redux

---
+++ client/src/actions/auth.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+import{
+    REGISTER_SUCCESS,
+    REGISTER_FAIL
+} from './type';
+
+//Register User
+export const register = ({name,email,password}) => dispatch =>{
+    
+}
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,10 @@
 import React, {Fragment, useState} from 'react';
+import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
+import {setAlert} from '../../actions/alert';
+import PropTypes from 'prop-types';
 
-const Register = () => {
+const Register = ({setAlert}) => {
     const [formData, setFormData] = useState({
         name:'',
         email:'',
@@ -16,7 +19,7 @@ const Register = () => {
     const onSubmit = async  e =>{
         e.preventDefault();
         if (password !== password2){
-            console.log('Password do not match');
+            setAlert('Password do not match', ' danger',3000);
         }else{
             console.log('success');
         }
@@ -80,6 +83,13 @@ const Register = () => {
             </p> 
         </Fragment>
     )
-}
+};
 
-export default Register
+Register.propTypes = {
+    setAlert: PropTypes.func.isRequired,
+};
+
+export default connect(
+    null, 
+    {setAlert}
+)(Register);
